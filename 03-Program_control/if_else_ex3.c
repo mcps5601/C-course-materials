a353f188a2dff6ddacf3f1e086847a8bc3010231
mcps5601 <mcps5601@gmail.com>
2024-09-19 11:45:36 +0800

    feat: add one more example for if-else and revise ex2

--- 03-Program_control/if_else_ex3.c
+++ 03-Program_control/if_else_ex3.c
@@ -2,11 +2,9 @@
 int main(){
     int score;
     scanf("%d", &score);
-    if (score >= 60){
+    if (score >= 60)
         printf("分數為%d，", score);
         printf("Passed!");
-    }
     else
         printf("分數為%d，", score);
-        printf("Failed!"); // 這行一定會執行
 }
