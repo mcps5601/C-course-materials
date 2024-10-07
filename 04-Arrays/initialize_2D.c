3374e6ce4e85c47a2d35fb2791b6cca64634db8e
mcps5601 <mcps5601@gmail.com>
2024-10-07 12:16:27 +0800

    feat: update the code of 04-Arrays

--- 04-Arrays/initialize_2D.c
+++ 04-Arrays/initialize_2D.c
@@ -3,4 +3,9 @@ int main(){
         {80, 85, 90, 100}, 
         {80, 85, 90, 100}
     };
+    for (int row = 0; row < 2; row++){
+        for (int col = 0; col < 4; col++){
+            printf("%d at (%d, %d)\n", score[row][col], row, col);
+        }
+    }
 }
---
+++ 04-Arrays/observe_shape_3D.c
@@ -0,0 +1,30 @@
+int main(){
+    int score[3][2][4] = {
+        {
+            {80, 85, 90, 100}, 
+            {60, 65, 70, 100}
+        },
+        {
+            {80, 85, 90, 100}, 
+            {60, 65, 70, 100}
+        },
+        {
+            {80, 85, 90, 100}, 
+            {60, 65, 70, 100}
+        }
+    };
+    printf("The size of this array: %d\n", sizeof(score));
+    int total_elements = sizeof(score) / sizeof(score[0][0][0]);
+
+    // Size of the first dimension
+    int dim1 = sizeof(score) / sizeof(score[0]);
+
+    // Size of the second dimension
+    int dim2 = sizeof(score[0]) / sizeof(score[0][0]);
+
+    // Size of the third dimension
+    int dim3 = sizeof(score[0][0]) / sizeof(score[0][0][0]);
+
+    printf("The shape of the 3D array is: [%d][%d][%d]\n", dim1, dim2, dim3);
+    printf("Total elements: %d\n", total_elements);
+}
