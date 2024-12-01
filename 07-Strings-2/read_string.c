5694331f06ad8a1b7f6ced9b38c87e623a425cdd
mcps5601 <mcps5601@gmail.com>
2024-12-02 02:34:47 +0800

    feat: update code for strings

---
+++ 07-Strings-2/read_string.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+int main(void){
+    char str_to_load[100];
+    printf("Enter a string (C Programming): ");
+    // scanf("%s", str_to_load);
+    // fgets(str_to_load, 3, stdin);
+    printf("You entered: %s\n", str_to_load);
+}
